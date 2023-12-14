7acf1b326d0e5772eabd67fab4505ca3797d0469
Sharon Abramson <sharabramson@gmail.com>
2023-12-14 16:36:15 -0500

    updated content in server.js

--- server.js
+++ server.js
@@ -6,23 +6,23 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//Middleware for parsing JSON and urlencoded data
+//Middleware for parsing JSON and urlencoded data.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.use(express.static('public'));
 
-//GET route for landing page
+//GET route for landing page.
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-//Get route for notes page
+//Get route for notes page.
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
-);
+);    
